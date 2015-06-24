5bd5c03f0cfd364da203b614d4e2e2db8c111d8b
geilt <alex@geilt.com>
2015-06-24 16:19:47 -0600

    First Commit. Will make compatible with AMD later.

---
+++ jquery-api.js
@@ -0,0 +1,56 @@
+"use strict";
+
+var $ = require('jquery'),
+	_ = require('lodash');
+
+function api(){
+	this.server = 'http://localhost;
+	
+	this.settings = {
+		xhrFields: {
+	      	withCredentials: true
+	   }
+	};
+}
+
+api.prototype.config = function(args){
+	this.settings = $.extend(this.settings, args);
+};
+api.prototype.get = function(path, data, callback){
+	var url = this.server + '/' + path;
+	
+	var settings = _.clone(this.settings);
+	settings.method = 'GET';
+	settings.data = data || {};
+
+	return $.ajax(url, settings);
+};
+api.prototype.post = function(path, data, callback){
+	var url = this.server + '/' + path;
+	
+	var settings = _.clone(this.settings);
+	settings.method = 'POST';
+	settings.data = data || {};
+
+	return $.ajax(url, settings);
+};
+api.prototype.put = function(path, data, callback){
+	var url = this.server + '/' + path;
+	
+	var settings = _.clone(this.settings);
+	settings.method = 'PUT';
+	settings.data = data || {};
+
+	return $.ajax(url, settings);
+};
+api.prototype.delete = function(path, data, callback){
+	var url = this.server + '/' + path;
+	
+	var settings = _.clone(this.settings);
+	settings.method = 'DELETE';
+	settings.data = data || {};
+
+	return $.ajax(url, settings);
+};
+
+module.exports = new api();
